ee534f4ba3b472085f5144fcaf8642eae5d2dc77
Jose <jlaresti93@gmail.com>
2024-04-03 01:30:37 +0200

    Añadiendo el metodo actualizar utilizando el mismo formulario que crear

--- Angular/cursos/src/app/app.routes.ts
+++ Angular/cursos/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { UsersFormComponent } from './components/users/users-form.component';
 export const routes: Routes = [
     {path: 'students', component:UsersComponent},
     {path: 'students/form', component:UsersFormComponent},
+    {path: 'students/form/:id', component:UsersFormComponent},
     {path: 'courses', component:CoursesComponent},
     {path: 'exams', component:ExamsComponent},
     {path: '**', redirectTo: 'students', pathMatch: 'full'}
--- Angular/cursos/src/app/components/users/users-form.component.ts
+++ Angular/cursos/src/app/components/users/users-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { Student } from '../../models/student';
 import { StudentService } from '../../services/student.service';
 import { CommonModule } from '@angular/common';
@@ -8,7 +8,12 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-users-form',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, CommonModule, FormsModule],
+  imports: [
+    RouterLink, 
+    RouterOutlet, 
+    CommonModule, 
+    FormsModule
+  ],
   templateUrl: './users-form.component.html',
   styleUrl: './users-form.component.css'
 })
@@ -16,18 +21,52 @@ export class UsersFormComponent implements OnInit{
   
   titulo = "Inserta un alumnno";
   student: Student = new Student();
+  error: any;
 
   constructor(private service: StudentService, 
-    private router: Router) { }
+    private router: Router,
+    private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id: number = +params.get('id');
+      if(id){
+        
+        this.service.show(id).subscribe( student => {
+          this.student = student;
+          this.titulo = `Actualizando ${student.name}`;
+        });
+      }
+    })
   }
 
   public create(): void{
-    this.service.create(this.student).subscribe(student => {
-      console.log(student);
-      alert(`Alumno creado con exito ${student.name}`);
-      this.router.navigate(['/students']);
-    });
+    this.service.create(this.student).subscribe({
+      next: (student) => {
+        console.log(student);
+        alert(`Alumno creado con exito ${student.name}`);
+        this.router.navigate(['/students']);
+      },
+      error: err => {
+        if(err.status === 400){
+          this.error = err.error;
+          console.log(this.error);
+        }
+    }});
+  }
+
+  public edit(): void{
+    this.service.edit(this.student).subscribe({
+      next: (student) => {
+        console.log(student);
+        alert(`Alumno ${student.name} actualizado con exito`);
+        this.router.navigate(['/students']);
+      },
+      error: err => {
+        if(err.status === 400){
+          this.error = err.error;
+          console.log(this.error);
+        }
+    }});
   }
 }
