a2eff3e58c31fb60b40aa1e860eb0c4967e3ffbe
Jose <jlaresti93@gmail.com>
2024-04-02 23:36:45 +0200

    Mostrando la lista de usuarios en Angular

--- Angular/cursos/src/app/components/users/users.component.ts
+++ Angular/cursos/src/app/components/users/users.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
+import { StudentService } from '../../services/student.service';
+import { Student } from '../../models/student';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [AppComponent],
+  imports: [AppComponent, CommonModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent implements OnInit{
   
   titulo = 'Listado de Alumnos';
+  students: Student[];
+
+  constructor(private service: StudentService){}
   
   ngOnInit(): void {
+    this.service.list().subscribe(students => this.students = students);
   }
 
 
